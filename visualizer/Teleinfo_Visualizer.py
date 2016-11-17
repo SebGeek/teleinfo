a4fe5fd278ffa436504218c1e84198453df016a1
seb <famille.auray@gmail.com>
2016-11-17 21:51:33 +0100

    Update

--- visualizer/Teleinfo_Visualizer.py
+++ visualizer/Teleinfo_Visualizer.py
@@ -3,37 +3,29 @@
 
 #############################################
 #
-#
+# Teleinfo Visualizer
 #
 #############################################
+
+import os
+import csv
+
+# Tkinter
 try:
     from Tkinter import *
     from ScrolledText import *
     import tkMessageBox
 except:
     print "To install Tkinter: sudo apt-get install python-tk"
-
-import os
-import sys
-from Queue import Queue
-
+# Matplotlib
 import matplotlib
-matplotlib.use('TkAgg')
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
-from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+matplotlib.use('TkAgg')
 
-from numpy import arange, sin, pi
-
-
-# Access to libraries, equivalent to PYTHONPATH
-current_dir = os.path.dirname(os.path.realpath(__file__))
-sys.path.append(os.path.join(current_dir, "common/tools"))
-sys.path.append(os.path.join(current_dir, "common/core"))
-
-from TB_action import ActionGUI
-from TB_network_topology import ThreadReadStatus
 
 LOG_FRAME = False
+
 
 class Application(Frame):
 
@@ -61,61 +53,73 @@ class Application(Frame):
         menu.add_cascade(label="Help", menu=help_menu)
         help_menu.add_command(label="About...", command=self.about_command)
 
-        self.__createWidgets(self.root)
+        self.__create_widgets()
 
         # Update window graphics
         self.update()
 
-    def display(self, msg, erase=False, show=False):
+    def display(self, msg):
         if LOG_FRAME == True:
             self.textPad.insert('insert', msg + "\n")
             self.textPad.see('end')
 
         print msg
 
-        if erase == True:
-            self.display_text = ""
-        self.display_text += msg + "\n"
-        if show == True:
-            pass
-            #ObjGui = Gui(gui_root=self.root)
-            #ObjGui.popupWarning(text=self.display_text)
-            #ObjGui.close()
-
-    def __createWidgets(self, parent):
+    def __create_widgets(self):
         # Define a weight for automatic resize of components
-        parent.columnconfigure(0, weight=1)
+        self.root.columnconfigure(0, weight=1)
         row_current = 0
 
         # Plot
-        parent.rowconfigure(row_current, weight=1)
-
-        f = Figure(figsize=(5, 4), dpi=100)
-        a = f.add_subplot(111)
-        t = arange(0.0, 3.0, 0.01)
-        s = sin(2 * pi * t)
-        a.plot(t, s)
-
-        canvas = FigureCanvasTkAgg(f, master=parent)
-        canvas.get_tk_widget().grid(row=row_current, column=0, sticky="nsew")
-        toolbar = NavigationToolbar2TkAgg(canvas, parent)
+        self.fig = plt.figure(figsize=(5, 4), dpi=100)
+        self.root.rowconfigure(row_current, weight=1)
+        self.canvas = FigureCanvasTkAgg(self.fig, master=self.root)
+        self.canvas.get_tk_widget().grid(row=row_current, column=0, sticky="nsew")
+        toolbar = NavigationToolbar2TkAgg(self.canvas, self.root)
         toolbar.grid(row=row_current, column=0, sticky="nw")
+
+        self.plot_figure()
 
         # Log_frame
         if LOG_FRAME == True:
             row_current += 1
-            parent.rowconfigure(row_current, weight=1)
-            self.Log_frame = LabelFrame(parent, text='Log', padx=2, pady=2)
+            self.root.rowconfigure(row_current, weight=1)
+            self.Log_frame = LabelFrame(self.root, text='Log', padx=2, pady=2)
             self.Log_frame.rowconfigure(0, weight=1)
             self.Log_frame.grid(row=row_current, column=0, sticky = NSEW, padx=5, pady=5)
-
             self.Log_frame.columnconfigure(0, weight=1)
-
             self.textPad = ScrolledText(self.Log_frame, width=110, height=10)
             self.textPad.grid(row=0, column=0)
 
+    def plot_figure(self):
+        with open("../log/log.csv.2016-03-15", 'rb') as csvfile:
+            reader = csv.reader(csvfile, delimiter=";")
+
+            x_values = []
+            y_values = []
+            index = 0
+            for row in reader:
+                if "," in row[1]:
+                    x_values.append(index)
+                    y_values.append(float(row[1].replace(",", ".")))
+                    index += 1
+
+        plt.xlabel("Date")
+        plt.ylabel("Puissance (Watt)")
+        subplot = self.fig.add_subplot(111)
+        subplot.plot(x_values, y_values, label="Jour 1")
+        subplot.legend()
+        subplot.grid(True)
+
+        self.canvas.draw()
+
     def refresh_all(self):
         self.display("refresh_all")
+
+        self.plot_figure()
+
+        # Update window graphics
+        self.update()
 
     def about_command(self):
         tkMessageBox.showinfo("About", "Teleinfo visualizer\n\nS. Auray\n14/11/2016")
@@ -123,7 +127,7 @@ class Application(Frame):
 
 if __name__ == '__main__':
     root = Tk()
-    root.geometry("1000x800")
+    root.geometry("800x600")
 
     current_dir = os.path.dirname(os.path.realpath(__file__))
     icon_path = os.path.join(current_dir, 'icon.gif')
