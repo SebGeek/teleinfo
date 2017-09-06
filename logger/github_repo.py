b85dcf8c5bb4070a92f87390150758f0a084b711
seb <famille.auray@gmail.com>
2017-09-06 22:09:57 +0200

    up

--- logger/github_repo.py
+++ logger/github_repo.py
@@ -7,6 +7,8 @@ g = Github("SebGeek", "MonTele12")
 for repo in g.get_user().get_repos():
     if repo.name == "teleinfo":
         print repo
+        fd = open("../log/log.csv.2016-11-21", "r")
+        repo.create_file("/log/new_file2.txt", "add log", fd.read())
 
 if __name__ == "__main__":
     pass
